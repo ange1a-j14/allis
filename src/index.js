0fb942fd09f5f811ea32b9da481db08c4570a143
Angela Jia <42385351+22angiej@users.noreply.github.com>
2020-11-07 00:30:32 -0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import banner from './img/stanford.png';
+import Grid from '@material-ui/core/Grid';
+import Menu from '@material-ui/core/Menu';
+//import reportWebVitals from './reportWebVitals';
 
+class Header extends React.Component {
+   grid() {
+    return (
+      <Grid>
+        <Grid item xs={2}>
+          <button className="home-button"> Stan4d </button>
+        </Grid>
+        <Grid item xs={2}>
+          <h3> Welcome to Stan4d </h3>
+        </Grid>
+      </Grid>
+    )
+  }
+
+
+  render() {
+    return (
+      <>
+        <div className="banner">
+          <div className="banner-box">
+
+          </div>
+        </div>
+      </>
+    )
+  }
+}
+
+// ===========================================
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +46,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
